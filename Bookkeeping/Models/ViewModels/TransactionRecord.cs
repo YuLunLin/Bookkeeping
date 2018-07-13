6d3957be886d92f7545ed754ad3539f1c386e7ea
Billy <Billy@DESKTOP-L8KOEJP>
2018-07-13 22:09:55 +0800

    add 50 records

--- Bookkeeping/Models/ViewModels/TransactionRecord.cs
+++ Bookkeeping/Models/ViewModels/TransactionRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,16 @@ namespace Bookkeeping.Models.ViewModels
 {
     public class TransactionRecord
     {
+
+        [Display(Name = "類別")]
         public TransacionType Type { get; set; }
+
+        [Display(Name = "日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime TransactionDate { get; set; }
+
+        [Display(Name = "金額")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
         public int Amount { get; set; }
 
          

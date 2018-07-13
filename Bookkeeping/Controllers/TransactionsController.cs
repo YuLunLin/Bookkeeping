c873734f0d20d68e53f9a8055640879bd25bce20
Billy <Billy@DESKTOP-L8KOEJP>
2018-07-13 21:55:50 +0800

    create fake data

---
+++ Bookkeeping/Controllers/TransactionsController.cs
@@ -0,0 +1,31 @@
+﻿using Bookkeeping.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookkeeping.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        // GET: Transactions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult ShowTransactionRecord()
+        {
+            var Record = new List<TransactionRecord>
+            {
+                new TransactionRecord{ Type=TransacionType.支出,TransactionDate = new DateTime(2018,7,13),Amount=3500},
+                new TransactionRecord{ Type=TransacionType.收入,TransactionDate = new DateTime(2018,7,14),Amount=250},
+                new TransactionRecord{ Type=TransacionType.支出,TransactionDate = new DateTime(2018,7,15),Amount=1200},
+
+            };
+            return View(Record);
+        }
+    }
+}
---
+++ Bookkeeping/Models/ViewModels/TransactionRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookkeeping.Models.ViewModels
+{
+    public class TransactionRecord
+    {
+        public TransacionType Type { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int Amount { get; set; }
+
+         
+    }
+
+    public enum TransacionType
+    {
+        收入,
+        支出
+    }
+}
